29cf3103a22948df83e2b47fa8ba3b37d7c82e0f
liruncong <liruncong2018@qq.com>
2022-02-27 20:58:43 +0800

    Test_Ts.py

---
+++ bsp/beaglebone/Test_Ts.py
@@ -0,0 +1,16 @@
+﻿# coding: utf-8
+import click
+
+@click.command(help="")
+@click.option("--output", "-o", required=True, type=click.Path(dir_okay=False, writable=True), help="output file path")
+def cli(output):
+    fo = open(output, "w")
+    fo.write(
+'''
+    void Test() {}
+'''
+    )
+    fo.close()
+
+if __name__ == "__main__":
+    cli()
